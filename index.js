06ee86e84d6283dd29c8bac8377d0c39b4766f16
Rafael Ferraz <140864570+RafaeldeAFerraz@users.noreply.github.com>
2024-09-30 01:26:26 +0000

    Instalação do MongoDB, conexão com banco de dados, criação Read All [GET]

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+
 const express = require('express')
+const { MongoClient } = require('mongodb')
+
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const dbUrl = "mongodb+srv://admin:KJsim29sjakjm437A@cluster0.1dfpx.mongodb.net"
+const dbName = "exercicio-deploy-e-segurança"
 
-app.listen(3000)
+
+async function main() {
+    const client = new MongoClient(dbUrl)
+    console.log('Conectando ao banco de dados...')
+    await client.connect()
+    console.log('Banco de dados conectado com sucesso!')
+
+    const db = client.db(dbName)
+    const collection = db.collection('evento')
+
+    
+
+    app.get('/', function (req, res) {
+        res.send('Hello World')
+    })
+
+    // Endpoint Read All [GET]
+    app.get('/evento',async function (req, res) {
+        const itens = await collection.find().toArray()
+
+        res.send(itens)
+    })
+
+
+    app.listen(3000)
+}
+
+main()
