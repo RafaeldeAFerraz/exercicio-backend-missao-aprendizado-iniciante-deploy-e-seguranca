f07ccc98df85d502bce04236d20ee689cf1fa283
Rafael Ferraz <140864570+RafaeldeAFerraz@users.noreply.github.com>
2024-10-04 02:19:53 +0000

    Crição do EndPoint Update [PUT]

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function main() {
     })
 
     // Endpoint Create [POST]
-    app.post("/evento", async function (req, res) {
+    app.post('/evento', async function (req, res) {
         const novoItem = req.body
 
         if(!novoItem.nome) {
@@ -62,7 +62,29 @@ async function main() {
         res.send(novoItem)
     })
     
+    app.put('/evento/:id', async function (req, res) {
+        const id = req.params.id
 
+        const novoItem = req.body
+
+        if(!novoItem.nome) {
+            return res.status(400).send('Corpo da requisição deve conter a propriedade `nome`')
+        }
+        if(!novoItem.data) {
+            return res.status(400).send('Corpo da requisição deve conter a propriedade `data`')
+        }
+        if(!novoItem.local) {
+            return res.status(400).send('Corpo da requisição deve conter a propriedade `local`')
+        }
+        
+        await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: novoItem }
+        )
+        
+        res.status(201).send(novoItem)
+    })
+    
     app.listen(3000)  
 }
 
